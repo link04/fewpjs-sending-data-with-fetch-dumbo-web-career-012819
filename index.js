f3274b9a53c3d15aed7b5c0a75ef672564df6b33
Maximo Bautista <max.bautista97@gmail.com>
2019-03-18 18:58:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
-const testVar = {}
 
-function testFunc() {
-  return "hi"
+let formData = {
+  "firstName": "Byron",
+  "registryMessage": "Poodle"
+};
+ 
+let configObj = {
+  method: "POST",
+headers: {
+      "Content-Type": "application/json",
+     "Accept" : "application/json"
+  },
+  body: JSON.stringify(formData)
 }
+
+const registerSelf = () => {
+ return  fetch("http://guestbook.example.com/register" , configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(json => {
+     return json.message;
+  })
+}
+
+const errorSelf = () => {
+ return  fetch("http://guestbook.example.com/register-error" , configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(json => {
+     return json.message;
+  }).catch(error => {
+    return ":(";
+  })
+}
+
+
